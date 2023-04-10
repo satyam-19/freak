18a7ff693aead397a549a3b09b9979f880530ced
satyam-19 <satyamchouksey21@gmail.com>
2023-04-10 22:31:16 +0530

    url encoded change

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:false}));
 app.use(express.static('assets'));
 
 
